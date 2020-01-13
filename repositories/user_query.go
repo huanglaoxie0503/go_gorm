94111eaf843eb511486669c393ed6fe0f140a3ff
袁承志 <huanghs@jiaolongchuhai.com>
2020-01-13 11:06:38 +0800

    gorm 更新操作

--- repositories/user_query.go
+++ repositories/user_query.go
@@ -16,8 +16,9 @@ func Connection() (*gorm.DB, error) {
 	}
 	return db, err
 }
+
 // 创建表
-func CreatTableOrm(db *gorm.DB)  {
+func CreatTableOrm(db *gorm.DB) {
 	if !db.HasTable(&model.User{}) {
 		if err := db.Set("grom:table_options", "ENGINE=InnoDB DEFAULT CHARSET=utf8").CreateTable(&model.User{}).Error; err != nil {
 			panic(err)
@@ -27,7 +28,7 @@ func CreatTableOrm(db *gorm.DB)  {
 }
 
 // 插入数据
-func InsertOrm(db *gorm.DB)  {
+func InsertOrm(db *gorm.DB) {
 	user := model.User{
 		Model:        gorm.Model{},
 		Name:         "oscar",
@@ -47,14 +48,14 @@ func InsertOrm(db *gorm.DB)  {
 	db.NewRecord(user)
 }
 
-func DeleteOrm(db *gorm.DB, hash uint64)  {
-	if err := db.Where(&model.LikeNum{Hash:hash}).Delete(model.LikeNum{}).Error; err != nil {
+func DeleteOrm(db *gorm.DB, hash uint64) {
+	if err := db.Where(&model.LikeNum{Hash: hash}).Delete(model.LikeNum{}).Error; err != nil {
 		fmt.Println(err)
 	}
 }
 
 // 查询
-func QueryOrm(db *gorm.DB, user *model.User)  {
+func QueryOrm(db *gorm.DB, user *model.User) {
 	// 根据主键查询第一条记录 -->	SELECT * FROM users ORDER BY id LIMIT 1;
 	db.First(&user)
 	// 随机获取一条记录 -->	SELECT * FROM users LIMIT 1;
@@ -68,7 +69,7 @@ func QueryOrm(db *gorm.DB, user *model.User)  {
 }
 
 // Where 条件查询
-func QueryWhereOrm(db *gorm.DB, user *model.User)  {
+func QueryWhereOrm(db *gorm.DB, user *model.User) {
 	// SELECT * FROM users WHERE name = 'oscar' limit 1;
 	db.Where("name = ?", "oscar").First(&user)
 
@@ -96,9 +97,9 @@ func QueryWhereOrm(db *gorm.DB, user *model.User)  {
 }
 
 // 当通过结构体进行查询时，GORM将会只通过非零值字段查询，这意味着如果你的字段值为0，''， false 或者其他 零值时，将不会被用于构建查询条件
-func QueryStructMapOrm(db *gorm.DB, user *model.User)  {
+func QueryStructMapOrm(db *gorm.DB, user *model.User) {
 	// Struct	SELECT * FROM users WHERE name = "oscar" AND age = 30 LIMIT 1;
-	db.Where(&model.User{Name:"oscar", Age:30}).First(&user)
+	db.Where(&model.User{Name: "oscar", Age: 30}).First(&user)
 
 	// Map	SELECT * FROM users WHERE name = "oscar" AND age = 30;
 	db.Where(map[string]interface{}{"name": "oscar", "age": 30}).Find(&user)
@@ -108,12 +109,12 @@ func QueryStructMapOrm(db *gorm.DB, user *model.User)  {
 }
 
 // Not 条件
-func QueryNotOrm(db *gorm.DB, user *model.User)  {
+func QueryNotOrm(db *gorm.DB, user *model.User) {
 	// SELECT * FROM users WHERE name <> "oscar" LIMIT 1;
 	db.Not("name", "oscar").First(&user)
 
 	// SELECT * FROM users WHERE id NOT IN (1,2,3);	Not In slice of primary keys
-	db.Not([]int64{1,2,3}).First(&user)
+	db.Not([]int64{1, 2, 3}).First(&user)
 
 	// SELECT * FROM users;
 	db.Not([]int64{}).First(&user)
@@ -122,23 +123,23 @@ func QueryNotOrm(db *gorm.DB, user *model.User)  {
 	db.Not("name = ?", "oscar").First(&user)
 
 	// SELECT * FROM users WHERE name <> "oscar";
-	db.Not(model.User{Name:"oscar"}).First(&user)
+	db.Not(model.User{Name: "oscar"}).First(&user)
 }
 
 // Or 条件
-func QueryOrOrm(db *gorm.DB, user *model.User)  {
+func QueryOrOrm(db *gorm.DB, user *model.User) {
 	// SELECT * FROM users WHERE role = 'admin' OR role = 'super_admin';
 	db.Where("role = ?", "admin").Or("role = ?", "super_admin").Find(&user)
 
 	// Struct
-	db.Where("name = ?").Or(model.User{Name:"oscar_2"}).Find(&user)
+	db.Where("name = ?").Or(model.User{Name: "oscar_2"}).Find(&user)
 
 	// Map	SELECT * FROM users WHERE name = 'oscar' OR name = 'oscar_2';
 	db.Where("name = 'oscar'").Or(map[string]interface{}{"name": "oscar_2"}).Find(&user)
 }
 
 // Inline Condition 内联条件
-func QueryConditionOrm(db *gorm.DB, user *model.User)  {
+func QueryConditionOrm(db *gorm.DB, user *model.User) {
 	// 根据主键获取记录 (只适用于整形主键)	SELECT * FROM users WHERE id = 23 LIMIT 1;
 	db.First(&user, 23)
 
@@ -149,25 +150,25 @@ func QueryConditionOrm(db *gorm.DB, user *model.User)  {
 	db.Find(&user, "name = ?", "oscar")
 
 	// Struct SELECT * FROM users WHERE age = 30;
-	db.Find(&user, model.User{Age:30})
+	db.Find(&user, model.User{Age: 30})
 
 	// Map  SELECT * FROM users WHERE age = 30;
 	db.Find(&user, map[string]interface{}{"age": 30})
 }
 
 // 其它查询选项
-func QueryExtraQueryingOption(db *gorm.DB, user *model.User)  {
+func QueryExtraQueryingOption(db *gorm.DB, user *model.User) {
 	// 为查询 SQL 添加额外的 SQL 操作	SELECT * FROM users WHERE id = 10 FOR UPDATE;
 	db.Set("gorm:query_option", "FOR UPDATE").First(&user, 10)
 }
 
 // 获取匹配的第一条记录，否则根据给定的条件初始化一个新的对象 (仅支持 struct 和 map 条件)
-func QueryFirstOrInit(db *gorm.DB, user *model.User)  {
+func QueryFirstOrInit(db *gorm.DB, user *model.User) {
 	// 未找到	user -> User{Name: "non_existing"}
-	db.FirstOrInit(&user, model.User{Name:"non existing"})
+	db.FirstOrInit(&user, model.User{Name: "non existing"})
 
 	// 找到	user -> User{Id: 1, Name: "oscar", Age: 30}
-	db.Where(model.User{Name:"oscar"}).FirstOrInit(&user)
+	db.Where(model.User{Name: "oscar"}).FirstOrInit(&user)
 
 	// user -> User{Id: 111, Name: "oscar", Age: 30}
 	db.FirstOrInit(&user, map[string]interface{}{"name": "oscar"})
@@ -175,61 +176,61 @@ func QueryFirstOrInit(db *gorm.DB, user *model.User)  {
 }
 
 // 如果记录未找到，将使用参数初始化 struct
-func QueryAttrsOrm(db *gorm.DB, user *model.User)  {
+func QueryAttrsOrm(db *gorm.DB, user *model.User) {
 	// 未找到	SELECT * FROM USERS WHERE name = 'non_existing';	user -> User{Name: "non_existing", Age: 30}
-	db.Where(model.User{Name:"non existing"}).Attrs(model.User{Age:30}).FirstOrInit(&user)
+	db.Where(model.User{Name: "non existing"}).Attrs(model.User{Age: 30}).FirstOrInit(&user)
 	// SELECT * FROM USERS WHERE name = 'non_existing';		user -> User{Name: "non_existing", Age: 30}
-	db.Where(model.User{Name:"non existing"}).Attrs("age", 30).FirstOrInit(&user)
+	db.Where(model.User{Name: "non existing"}).Attrs("age", 30).FirstOrInit(&user)
 
 	// 找到	SELECT * FROM USERS WHERE name = oscar';	user -> User{Id: 111, Name: "oscar", Age: 30}
-	db.Where(model.User{Name:"oscar"}).Attrs(model.User{Age:30}).FirstOrInit(&user)
+	db.Where(model.User{Name: "oscar"}).Attrs(model.User{Age: 30}).FirstOrInit(&user)
 }
 
 // 不管记录是否找到，都将参数赋值给 struct
-func QueryAssignOrm(db *gorm.DB, user *model.User)  {
+func QueryAssignOrm(db *gorm.DB, user *model.User) {
 	// 未找到 	user -> User{Name: "non_existing", Age: 20}
-	db.Where(model.User{Name:"non_existing"}).Assign(model.User{Age:30}).FirstOrInit(&user)
+	db.Where(model.User{Name: "non_existing"}).Assign(model.User{Age: 30}).FirstOrInit(&user)
 
 	// 找到	SELECT * FROM USERS WHERE name = oscar';	user -> User{Id: 111, Name: "oscar", Age: 20}
-	db.Where(model.User{Name:"oscar"}).Assign(model.User{Age:30}).FirstOrInit(&user)
+	db.Where(model.User{Name: "oscar"}).Assign(model.User{Age: 30}).FirstOrInit(&user)
 }
 
 // 获取匹配的第一条记录, 否则根据给定的条件创建一个新的记录 (仅支持 struct 和 map 条件)
-func QueryFirstOrCreateOrm(db *gorm.DB, user *model.User)  {
+func QueryFirstOrCreateOrm(db *gorm.DB, user *model.User) {
 	// 未找到	INSERT INTO "users" (name) VALUES ("non_existing");	user -> User{Id: 112, Name: "non_existing"}
-	db.FirstOrCreate(&user, model.User{Name:"non_existing"})
+	db.FirstOrCreate(&user, model.User{Name: "non_existing"})
 
 	// 找到	user -> User{Id: 111, Name: "user"}
-	db.Where(model.User{Name:"oscar"}).FirstOrCreate(&user)
+	db.Where(model.User{Name: "oscar"}).FirstOrCreate(&user)
 
 	// 1. Attrs 如果记录未找到，将使用参数创建 struct 和记录
 	// (1). 未找到
-		// SELECT * FROM users WHERE name = 'non_existing';
-		// INSERT INTO "users" (name, age) VALUES ("non_existing", 30);
-		// user -> User{Id: 1, Name: "non_existing", Age: 30}
-	db.Where(model.User{Name:"non_existing"}).Attrs(model.User{Age:30}).FirstOrCreate(&user)
+	// SELECT * FROM users WHERE name = 'non_existing';
+	// INSERT INTO "users" (name, age) VALUES ("non_existing", 30);
+	// user -> User{Id: 1, Name: "non_existing", Age: 30}
+	db.Where(model.User{Name: "non_existing"}).Attrs(model.User{Age: 30}).FirstOrCreate(&user)
 
 	// (2). 找到
-		// SELECT * FROM users WHERE name = 'oscar';
-		// user -> User{Id: 111, Name: "oscar", Age: 20}
-	db.Where(model.User{Name:"oscar"}).Attrs(model.User{Age:30}).FirstOrCreate(&user)
+	// SELECT * FROM users WHERE name = 'oscar';
+	// user -> User{Id: 111, Name: "oscar", Age: 20}
+	db.Where(model.User{Name: "oscar"}).Attrs(model.User{Age: 30}).FirstOrCreate(&user)
 
 	// 2. Assign 不管记录是否找到，都将参数赋值给 struct 并保存至数据库.
 	// (1).未找到
-		// SELECT * FROM users WHERE name = 'non_existing';
-		// INSERT INTO "users" (name, age) VALUES ("non_existing", 30);
-		// user -> User{Id: 112, Name: "non_existing", Age: 30}
-	db.Where(model.User{Name:"non_existing"}).Assign(model.User{Age:30}).FirstOrCreate(&user)
+	// SELECT * FROM users WHERE name = 'non_existing';
+	// INSERT INTO "users" (name, age) VALUES ("non_existing", 30);
+	// user -> User{Id: 112, Name: "non_existing", Age: 30}
+	db.Where(model.User{Name: "non_existing"}).Assign(model.User{Age: 30}).FirstOrCreate(&user)
 
 	// (2). 找到
-		// SELECT * FROM users WHERE name = 'oscar';
-		// UPDATE users SET age=30 WHERE id = 111;
-		// user -> User{Id: 111, Name: "oscar", Age: 30}
-	db.Where(model.User{Name:"oscar"}).Assign(model.User{Age:30}).FirstOrCreate(&user)
+	// SELECT * FROM users WHERE name = 'oscar';
+	// UPDATE users SET age=30 WHERE id = 111;
+	// user -> User{Id: 111, Name: "oscar", Age: 30}
+	db.Where(model.User{Name: "oscar"}).Assign(model.User{Age: 30}).FirstOrCreate(&user)
 }
 
 // 高级查询
-func QueryAdvancedOrm(db *gorm.DB, order *model.Order)  {
+func QueryAdvancedOrm(db *gorm.DB, order *model.Order) {
 	// 1. SubQuery 子查询  基于 *gorm.expr 的子查询
 	// SELECT * FROM "order"  WHERE "order"."deleted_at" IS NULL AND (amount > (SELECT AVG(amount) FROM "order"  WHERE (state = 'paid')));
 	db.Where("amount > ?", db.Table("order").Select("AVG(amount)").Where("state = ?", "paid").QueryExpr()).Find(&order)
@@ -287,35 +288,67 @@ func QueryAdvancedOrm(db *gorm.DB, order *model.Order)  {
 	db.Table("deleted_users").Count(&count)
 
 	// SELECT count( distinct(name) ) FROM deleted_users;
+	// Count 必须是链式查询的最后一个操作 ，因为它会覆盖前面的 SELECT，但如果里面使用了 count 时不会覆盖
 	db.Table("deleted_users").Select("count(distinct(name))").Count(&count)
 }
 
+// Group & Having
+func QueryGroupHaving(db *gorm.DB, order *model.Order) {
+	rows1, err1 := db.Table("order").Select("date(created_at) as date, sum(amount) as total").Group("date(created_at)").Rows()
+	if err1 != nil {
+		fmt.Println(err1)
+	}
+	for rows1.Next() {
 
+	}
 
-func UpdateOrm(db *gorm.DB, like *model.LikeNum)  {
-	// db.Model() 选择一个表
-	// db.Where() 构造查询条件
-	// db.Count() 计算数量
-	// db.Find(&Likes)  获取对象
-	// db.First(&Like)  查一条记录
-	db.Model(&like).Update("Title", "hello")
-	db.Model(&like).Updates(model.LikeNum{Title:"oscar", Ip:"192.168.31.82"})
-	db.Model(&like).Updates(model.LikeNum{Title:"root", Ip:"123", Ua:""})
+	rows2, err2 := db.Table("order").Select("date(created_at) as date, sum(amount) as total").Group("date(created_at)").Having("sum(amount) > ?", 100).Rows()
+	if err2 != nil {
+		fmt.Println(err2)
+	}
+	for rows2.Next() {
+
+	}
+
+	db.Table("order").Select("date(created_at) as date, sum(amount) as total").Group("date(created_at)").Having("sum(amount) > ?", 100).Scan(&order)
 }
 
-func CreateAnimals(db *gorm.DB)  {
-	// db.Begin() 声明开启事务
-	cursor := db.Begin()
-	if err := cursor.Create(&model.LikeNum{Title: "hadria"}).Error; err != nil {
-		// 异常的时候调用
-		cursor.Rollback()
+func QueryJoinsOrm(db *gorm.DB, order *model.Order) {
+	rows, err := db.Table("user").Select("user.name, email.email").Joins("left join email.user_id = user.id").Rows()
+	if err != nil {
 		fmt.Println(err)
 	}
-	if err := cursor.Create(&model.LikeNum{Title: "hello"}).Error; err != nil {
-		cursor.Rollback()
-		fmt.Println(err)
+	for rows.Next() {
+
 	}
-	// 结束的时候调用
-	cursor.Commit()
+
+	db.Table("user").Select("user.name, email.email").Joins("left join email on email.user_id = user.id").Scan(&order)
+
+	db.Joins("JOIN emails ON emails.user_id = user.id AND emails.email = ?", "oscar@qq.com").Joins("JOIN credit_cards ON credit_cards.user_id = user.id").Where("credit_cards.number = ?", 110).Find(&order)
 }
 
+// Pluck，查询 model 中的一个列作为切片，如果您想要查询多个列，您应该使用 Scan
+func QueryPluckOrm(db *gorm.DB, user *model.User) {
+	var ages []int64
+	db.Find(&user).Pluck("age", &ages)
+
+	var names []string
+	db.Model(&model.User{}).Pluck("name", &names)
+
+	db.Table("deleted_users").Pluck("name", &names)
+
+	// 查询多个字段
+	db.Select("name, age").Find(&user)
+}
+
+// Scan，扫描结果至一个 struct
+func QueryScanOrm(db *gorm.DB) {
+	type Result struct {
+		Name string
+		Age  int
+	}
+	var result Result
+	db.Table("user").Select("name, age").Where("name = ?", "oscar").Scan(&result)
+
+	db.Raw("select name, age from user where name = ?", "oscar").Scan(&result)
+}
---
+++ repositories/user_update.go
@@ -0,0 +1,91 @@
+package repositories
+
+import (
+	"fmt"
+	"github.com/jinzhu/gorm"
+	"go_gorm/model"
+)
+
+// Save会更新所有字段，即使你没有赋值
+func UpdateSaveOrm(db *gorm.DB, user *model.User) {
+	db.First(&user)
+
+	user.Name = "oscar"
+	user.Age = 30
+	db.Save(&user)
+}
+
+// 更新指定字段，可以使用Update或者Updates
+func UpdateFieldOrm(db *gorm.DB, user *model.User) {
+	// 更新单个属性，如果它有变化
+	// UPDATE user SET name='oscar', updated_at='2019-11-17 21:34:10' WHERE id=110;
+	db.Model(&user).Update("name", "oscar")
+
+	// 根据给定的条件更新单个属性
+	// UPDATE users SET name='oscar', updated_at='2019-11-17 21:34:10' WHERE id=111 AND active=true;
+	db.Model(&user).Where("active = ?", true).Update("name", "oscar")
+
+	// 使用 map 更新多个属性，只会更新其中有变化的属性
+	// UPDATE users SET name='oscar', age=18, active=false, updated_at='2019-11-17 21:34:10' WHERE id=111;
+	db.Model(&user).Updates(map[string]interface{}{"name": "oscar", "age": 30, "active": false})
+
+	// 使用 struct 更新多个属性，只会更新其中有变化且为非零值的字段
+	// UPDATE users SET name='oscar', age=18, updated_at = '2019-11-17 21:34:10' WHERE id = 111;
+	db.Model(&user).Updates(model.User{Name: "oscar", Age: 18})
+
+	/*警告：当使用 struct 更新时，GORM只会更新那些非零值的字段*/
+	// 对于下面的操作，不会发生任何更新，"", 0, false 都是其类型的零值
+	db.Model(&user).Updates(model.User{Name: "", Age: 30})
+}
+
+// 更新选定字段	如果你想更新或忽略某些字段，你可以使用 Select，Omit
+func UpdateSelectFieldOrm(db *gorm.DB, user *model.User) {
+	// UPDATE users SET name='oscar', updated_at='2019-11-17 21:34:10' WHERE id=111;
+	db.Model(&user).Select("name").Updates(map[string]interface{}{"name": "oscar", "age": 30})
+
+	// UPDATE users SET age=18, name="oscar", updated_at='2019-11-17 21:34:10' WHERE id=111;
+	db.Model(&user).Omit("name").Updates(map[string]interface{}{"name": "oscar", "age": 30})
+}
+
+// 无 Hooks 更新
+func UpdateHooksOrm(db *gorm.DB, user *model.User) {
+	// 更新单个属性，类似于 `Update`
+	// UPDATE users SET name='oscar' WHERE id = 111;
+	db.Model(&user).UpdateColumn("name", "oscar")
+
+	// 更新多个属性，类似于 `Updates`
+	// UPDATE user SET name='oscar', age=18 WHERE id = 111;
+	db.Model(&user).UpdateColumns(model.User{Name: "oscar", Age: 20})
+}
+
+// 批量更新 批量更新时 Hooks 不会运行
+func UpdateBatchOrm(db *gorm.DB, user *model.User) {
+	// UPDATE users SET name='oscar', age=18 WHERE id IN (10, 11);
+	db.Table("user").Where("id IN (?)", []int{10, 11}).Updates(map[string]interface{}{"name": "oscar", "age": 20})
+
+	/*使用 struct 更新时，只会更新非零值字段，若想更新所有字段，请使用map[string]interface{}*/
+	// UPDATE users SET name='oscar', age=30;
+	db.Model(model.User{}).Updates(model.User{Name: "oscar", Age: 30})
+
+	// 使用 `RowsAffected` 获取更新记录总数
+	i := db.Model(model.User{}).Updates(model.User{Name: "oscar", Age: 20}).RowsAffected
+	fmt.Println(i)
+}
+
+func UpdateSqlExpOrm(db *gorm.DB, order *model.Order) {
+	// UPDATE "order" SET "price" = price * '2' + '100', "updated_at" = '2019-11-17 21:34:10' WHERE "id" = '2';
+	db.Model(&order).Update("price", gorm.Expr("price * ? + ?", 2, 100))
+
+	// UPDATE "order" SET "price" = price * '2' + '100', "updated_at" = '2019-11-17 21:34:10' WHERE "id" = '2';
+	db.Model(&order).Updates(map[string]interface{}{"price": gorm.Expr("price * ? + ?", 2, 100)})
+
+	// UPDATE "order" SET "quantity" = quantity - 1 WHERE "id" = '2' AND quantity > 1;
+	db.Model(&order).Where("quantity > 1").UpdateColumn("quantity", gorm.Expr("quantity - ?", 1))
+}
+
+func UpdateOtherOrm(db *gorm.DB, user *model.User) {
+	// 为 update SQL 添加其它的 SQL
+	db.Model(&user).Set("gorm:update_option", "OPTION (OPTIMIZE FOR UNKNOWN)").Update("name", "oscar")
+	//// UPDATE users SET name='hello', updated_at = '2013-11-17 21:34:10' WHERE id=111 OPTION (OPTIMIZE FOR UNKNOWN);
+
+}
